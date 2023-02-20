b7c6fa041d712e4e3e8d0b769b35d4ee6dddeef9
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-02-20 13:51:23 -0500

    Update 1299. Replace Elements with Greatest Element on Right Side.cpp

--- C++ code/1299. Replace Elements with Greatest Element on Right Side.cpp
+++ C++ code/1299. Replace Elements with Greatest Element on Right Side.cpp
@@ -1,1 +1,3 @@
-https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+# link : https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+# Reverse order calculation to avoid iterating and checking every value in the array, whihc takes up memory = O(n^2)
+
