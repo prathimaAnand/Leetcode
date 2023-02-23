1a31b32d481c604b0dcaf50520e701d6dbf107b5
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-02-22 23:09:13 -0500

    Update is subsequence.py

--- all arrays/is subsequence.py
+++ all arrays/is subsequence.py
@@ -1,1 +1,38 @@
-#link: https://leetcode.com/problems/is-subsequence/
+#https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i]  == t[j]:
+                i+= 1
+                j+=
+            else:
+                j+= 1
+
+        if i == len(s) :
+            return True
+        else :
+            False
+        
+Memory = 14 MB beats = 76%
+Runtime = 46 ms = 16.5%
+            
+#  Simplified version
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i]  == t[j]:
+                i+= 1
+            # //     j+=
+            # // else
+            j+= 1
+
+        return True if i == len(s) else False
+
+      Memory = 13.8 MB beats = 98.5%
+Runtime = 44 ms = %
