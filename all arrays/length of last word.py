dbcb33f64eae6c746179ed0a2e2e0340ca17f032
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-02-23 22:07:10 -0500

    Update length of last word.py

--- all arrays/length of last word.py
+++ all arrays/length of last word.py
@@ -1,3 +1,5 @@
+https://leetcode.com/problems/length-of-last-word/submissions/903894875/
+
 Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 A word is a maximal substring consisting of non-space characters only.
