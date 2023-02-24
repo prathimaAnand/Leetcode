f4fff460275a48e1237a7105cf8b5cb2b20c41eb
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-02-23 22:18:09 -0500

    Update length of last word.cpp

--- C++ code/length of last word.cpp
+++ C++ code/length of last word.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+      int i = s.length() - 1, length = 0;  
+    //   cout<<i;
+        while (s.isspace() == true):
+            i -= 1;
+        while (i > = 0 && s[i] != " "):
+            length += 1;
+            i -= 1;
+    return i;
+    }
+};
