408667d79d816d5f7113e8f7bf435647be4f7060
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-02-26 02:02:26 -0500

    Update longest common prefx.py

--- all arrays/longest common prefx.py
+++ all arrays/longest common prefx.py
@@ -1,1 +1,17 @@
+https://leetcode.com/problems/longest-common-prefix/submissions/905169121/
 
+  
+ logic: compare 1st string with all others strings
+  edge case: if 2nd string goes out of bounds then we should return the result
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        result = ""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return result
+            result += strs[0][i]
+        return result
