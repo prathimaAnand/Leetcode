420053098669b5ccd13221d79f15d11e78f857c3
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-02 23:54:48 -0500

    Update valid palindrome.py

--- Blind75/valid palindrome.py
+++ Blind75/valid palindrome.py
@@ -4,12 +4,12 @@ https://leetcode.com/problems/valid-palindrome/submissions/908115575/
     
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        NewString = ''  # increases memory complexity
+        NewString = ''  # extra memory
 
         for i in s:
             if i.isalnum():
                 NewString += i.lower()
-        return NewString == NewString[::-1]
+        return NewString == NewString[::-1] # extra memory
     
 #Solution 2 : without using built-in functions
                  
