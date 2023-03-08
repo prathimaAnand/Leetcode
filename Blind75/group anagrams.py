edcc607e4657ec8643037938f70b74709b441341
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-07 23:19:32 -0500

    Update group anagrams.py

--- Blind75/group anagrams.py
+++ Blind75/group anagrams.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        FinalResult = defaultdict(list)
+
+        for i in strs:
+            count = [0] * 26
+
+            for j in i:
+                count[ord(j) - ord("a")] += 1
+
+            FinalResult[tuple(count)].append(i)
+           
+        return FinalResult.values()
