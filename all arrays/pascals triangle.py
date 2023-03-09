149b37bb1b40f5dbee7a16bd75490379db3cff73
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-08 19:58:57 -0500

    Update pascals triangle.py

--- all arrays/pascals triangle.py
+++ all arrays/pascals triangle.py
@@ -1,1 +1,17 @@
 
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        result2Darr = [[1]]
+
+        for i in range(numRows - 1):
+
+            tempRow = [0] + result2Darr[-1] +[0]
+            row =[]
+
+            for j in range(len(result2Darr[-1]) + 1):
+                row.append(tempRow[j] + tempRow[j+1])
+            result2Darr.append(row)
+
+        return result2Darr
