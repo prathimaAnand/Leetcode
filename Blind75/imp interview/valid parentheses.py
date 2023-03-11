588010ccc8a0d0d3d85438b93d1efb8968d5e119
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-10 22:49:08 -0500

    Update valid parentheses.py

--- Blind75/imp interview/valid parentheses.py
+++ Blind75/imp interview/valid parentheses.py
@@ -7,3 +7,20 @@ when a closed bracket is encountered check haspmap for value and match them with
 
 memory & time = O(n)
 '''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stackList = []
+        bracketHM = { ")" : "(", "}" : "{",  "]" : "[" }
+
+        for i in s:
+            if i in bracketHM:
+                if stackList and stackList[-1] == bracketHM[i]:
+                    stackList.pop()
+                else:
+                    return False
+            else:
+                stackList.append(i) # if you open bracket is encountered
+        
+        return True if not stackList else False # only enter true is stack is empty
+
