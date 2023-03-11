607a5940786bf035c823d6751a667da728493aee
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-10 19:59:12 -0500

    Update best time to buy and sell stock.py

--- Blind75/sliding window/best time to buy and sell stock.py
+++ Blind75/sliding window/best time to buy and sell stock.py
@@ -13,3 +13,20 @@ memory = O(1) scan through array and 2 pointers
 time = O(1)
 
 '''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        leftPointer, rightPointer = 0, 1
+        maxProfit = 0
+
+        while rightPointer < len(prices):
+            if prices[leftPointer] < prices[rightPointer]:
+                currentProfit = prices[rightPointer] - prices[leftPointer]
+                maxProfit = max(currentProfit, maxProfit)
+            else:
+                leftPointer = rightPointer
+
+            rightPointer +=1
+
+        return maxProfit
+
