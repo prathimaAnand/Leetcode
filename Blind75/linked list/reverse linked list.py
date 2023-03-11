a834f9df2480667db79d6cc2c8063152a3e45483
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-11 18:37:04 -0500

    Update reverse linked list.py

--- Blind75/linked list/reverse linked list.py
+++ Blind75/linked list/reverse linked list.py
@@ -1,9 +1,13 @@
 '''
-1. Two pointers
+1. Iteratively - Two pointers -->> Optimal solution
 prevPointer = null
 currentPointer = 1st element
 if currentPointer = 0 its end of the list
 and prevPointer will be the Head
 
-2. 
+M - O(1) 
+T - O(n)
+
+2. Recursive - 
+
 '''
