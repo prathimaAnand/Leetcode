968141dd5cb6ebcac8007a9fbaffd9bb4461a42d
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-11 18:53:08 -0500

    Update reverse linked list.py

--- Blind75/linked list/reverse linked list.py
+++ Blind75/linked list/reverse linked list.py
@@ -1,3 +1,5 @@
+# link : https://leetcode.com/problems/reverse-linked-list/submissions/913463419/
+
 '''
 1. Iteratively - Two pointers -->> Optimal solution
 prevPointer = null
@@ -9,5 +11,26 @@ M - O(1)
 T - O(n)
 
 2. Recursive - 
+takes more memory
+
+refer video
 
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previousP, currentP = None, head
+
+        while currentP:
+            temp = currentP.next # to save currentP.next separately
+            currentP.next = previousP # we are reversing the string
+            previousP = currentP # then incrementing
+            currentP = temp
+
+        return previousP
+             
