96c4ad5a145ccaae238b5776485b338c67e662ee
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-10 19:40:44 -0500

    Update best time to buy and sell stock.py

--- Blind75/sliding window/best time to buy and sell stock.py
+++ Blind75/sliding window/best time to buy and sell stock.py
@@ -1,1 +1,15 @@
+''' Concept : two pointers L & R
+L = Buy
+R = Sell
 
+Buy low and Sell high ( as high as possible)
+
+if L < R just incrememt R
+if L > R - increment both
+
+Max profit storage = min - max
+
+memory = O(1) 
+time = O(1)
+
+'''
