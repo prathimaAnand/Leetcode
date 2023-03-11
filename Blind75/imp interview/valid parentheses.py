b732d012a7cd88d6d1eb32fa3d55bbdb69198b29
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-10 21:46:10 -0500

    Update valid parentheses.py

--- Blind75/imp interview/valid parentheses.py
+++ Blind75/imp interview/valid parentheses.py
@@ -1,1 +1,9 @@
+'''
+Concept:
+using stack to store and pop characters
+hasp map stores - closed brackets as keys with open brackets as values
+when a closed bracket is encountered check haspmap for value and match them with going through stack
 
+
+memory & time = O(n)
+'''
