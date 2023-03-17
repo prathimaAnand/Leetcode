7720b4ee55846db90e6e646c7fa42b997637bb0d
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-16 20:43:26 -0400

    Update linked list cycle.py

--- Blind75/linked list/linked list cycle.py
+++ Blind75/linked list/linked list cycle.py
@@ -12,3 +12,19 @@ slow moves 1 step
 fast moves 2 step
 fast pointer will always meet slow pointer at the same time
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        
+        while fast and fast.next: # because fast pointer is swifted by 2 positions
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: # if they meet then cycle exists
+                return True
+        return False # out of bounce so no cycle
