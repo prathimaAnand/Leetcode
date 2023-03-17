9159ef32eb0ebb223d17684d00898fa32b5c78ea
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-16 20:37:56 -0400

    Update linked list cycle.py

--- Blind75/linked list/linked list cycle.py
+++ Blind75/linked list/linked list cycle.py
@@ -1,1 +1,14 @@
+2 ways to solve
+1. Hashset 
+storing node as it is, it will be used as objects
+memory & time = O(n)
+
+2. 2 pointers
+Memory = O(1)
+Time = O(n)
+
+slow and fast pointers
+slow moves 1 step
+fast moves 2 step
+fast pointer will always meet slow pointer at the same time
 
