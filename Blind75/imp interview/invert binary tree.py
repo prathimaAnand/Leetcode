1b086ddb3146ee6724827234f22f099f4414abbb
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-20 01:49:27 -0400

    Update invert binary tree.py

--- Blind75/imp interview/invert binary tree.py
+++ Blind75/imp interview/invert binary tree.py
@@ -4,6 +4,25 @@
 technique: Depth frist search
 in recrusive way
 
-
-
 '''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        # swap children
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
