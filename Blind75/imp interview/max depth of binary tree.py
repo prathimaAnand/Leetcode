5f64de350233c9a3f6e53d2cd08e84218c72395a
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-23 20:59:35 -0400

    Create max depth of binary tree.py

---
+++ Blind75/imp interview/max depth of binary tree.py
@@ -0,0 +1,23 @@
+# link : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+'''
+Three ways to solve
+'''
+# 1. RECURSIVE DFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        left_node = self.maxDepth(root.left)
+        right_node = self.maxDepth(root.right)
+        max_val = 1 + max(left_node, right_node)
+        return max_val
+
+
