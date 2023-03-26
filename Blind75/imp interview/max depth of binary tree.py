3bb145f4aa38654f647baa9239265aa4146c06bb
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-03-26 18:39:33 -0400

    Update max depth of binary tree.py

--- Blind75/imp interview/max depth of binary tree.py
+++ Blind75/imp interview/max depth of binary tree.py
@@ -53,4 +53,16 @@ class Solution:
         
  # 3. Iterative DFS
 
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        level = 0
+        stack = [[root, 1]]
 
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                level = max(level, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return level
