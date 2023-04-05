f45d80ba3c34a9a905b5927e3ffa9f6851dac1ac
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-05 18:24:28 -0400

    Update product of array except self.cpp

--- Blind75/algorithm/product of array except self.cpp
+++ Blind75/algorithm/product of array except self.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int numsSize = nums.size();
+        // int res[] = 1 * numsSize;
+        vector<int> res(nums.size(), 1);
 
+        int prefix = 1;
+        for(int i = 0; i<numsSize ; i++){
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for (int i = numsSize -1; i>=0 ; i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+        
+    }
+};
