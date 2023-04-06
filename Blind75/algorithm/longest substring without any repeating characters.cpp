21efc8f61bc02309dd2eedb24c694f8d76f854d3
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-05 20:09:02 -0400

    Update longest substring without any repeating characters.cpp

--- Blind75/algorithm/longest substring without any repeating characters.cpp
+++ Blind75/algorithm/longest substring without any repeating characters.cpp
@@ -1,1 +1,28 @@
+/*
+Once you get a duplicate stop searching for duplicates
+remove from left
+using "set" which does not contain duplicates
+Memory & Time - O(n)
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0, res = 0;
+        set<char> charSet;
+        
+        for(int r = 0; r < s.length(); r ++) {
+            while (charSet.find(s[r]) != charSet.end()) {
+                    charSet.erase(s[l]);
+                    l++;
+                }
+                charSet.insert(s[r]);
+                res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
+
+
+
 
