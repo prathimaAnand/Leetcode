08bd451c2474683a0ab3a4824d0f2461511f8b1f
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-12 19:19:36 -0400

    Update add two numbers.cpp

--- Blind75/algorithm/add two numbers.cpp
+++ Blind75/algorithm/add two numbers.cpp
@@ -1,4 +1,3 @@
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,10 +11,37 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy;
+        ListNode* dummy = new ListNode(0);
         ListNode* cur = dummy;
-        int carry = 0;
+        int carry = 0, v1, v2;
         
-        while()
+        while(l1 || l2 || carry){
+            if(v1)
+                v1 = l1->val;
+            else
+                NULL;
+            
+            if(v2)
+                v2 = l2->val;
+            else
+                NULL;
+            
+            int val = v1 + v2 + carry;
+            carry = val / 10;
+            val = val % 10;
+            cur->next = new ListNode(val);
+
+            //update pointers 
+            cur = cur->next;
+            if(l1)
+                l1 = l1->next;
+            else
+                NULL;
+            if(l2)
+                l2 = l2->next;
+            else
+                NULL;
+        }
+        return dummy->next;
     }
 };
