26f0e6f5fa4af6d47578b67f9c664dc0c6817c66
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-12 20:14:54 -0400

    Update construct binary tree from inorder and preorder traversal.cpp

--- C++ code/algorithms/construct binary tree from inorder and preorder traversal.cpp
+++ C++ code/algorithms/construct binary tree from inorder and preorder traversal.cpp
@@ -1,1 +1,8 @@
 
+/* Preorder traversal - root node -> left node -> right node
+Inorder traversal - start at root process left sub tree -> root - > right sub tree
+
+problem is solve for finding binary tree from these 2 traversals
+
+in the preorder we know what is the root and find that root in inorder and figure out left and right subtrees
+*/
