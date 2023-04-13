7cdaa11d77fcbc4d1bae19e547c68e9139ad384a
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-12 20:29:51 -0400

    Update construct binary tree from inorder and preorder traversal.py

--- Blind75/algorithms/construct binary tree from inorder and preorder traversal.py
+++ Blind75/algorithms/construct binary tree from inorder and preorder traversal.py
@@ -1,4 +1,4 @@
-//
+"""
 /* Preorder traversal - root node -> left node -> right node
 Inorder traversal - start at root process left sub tree -> root - > right sub tree
 
@@ -7,7 +7,7 @@ problem is solve for finding binary tree from these 2 traversals
 in the preorder we know what is the root and find that root in inorder and figure out left and right subtrees
 */
 
-//
+"""
 
 
 # Definition for a binary tree node.
