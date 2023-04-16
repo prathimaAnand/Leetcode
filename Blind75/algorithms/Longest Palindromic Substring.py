1c4b05712f94722ba4a600b52f84be011f0ba30f
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-15 22:04:45 -0400

    Update Longest Palindromic Substring.py

--- Blind75/algorithms/Longest Palindromic Substring.py
+++ Blind75/algorithms/Longest Palindromic Substring.py
@@ -7,4 +7,19 @@ keep a middle character and expand outward and check if they palindrome
 Edge case : It does not work for even palindromes
 fix it in code
 """
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        res = []
+        resLen = 0
 
+        for i in range(len(s))
+        # for odd set
+        l, r = i, i
+        while(l >= 0 and r <= len(s) and s[l] == s[r])
+            if(r - l + 1 > res)
+                res = s[l:r+1]
+
+
+    }
+};
