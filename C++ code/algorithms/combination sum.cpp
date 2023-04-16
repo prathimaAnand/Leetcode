4d0f76cd01983e62e2b8bd21203ebaccf3920c43
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-15 21:19:37 -0400

    Update combination sum.cpp

--- C++ code/algorithms/combination sum.cpp
+++ C++ code/algorithms/combination sum.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> cur;
 
+        dfs(candidates, target, res, cur, 0);
+
+        return res;
+    }
+
+private:
+    void dfs(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& cur, int i) {
+        if (target == 0) {
+            res.push_back(cur);
+            return;
+        }
+        if (target < 0 || i >= candidates.size()) {
+            return;
+        }
+
+        cur.push_back(candidates[i]);
+        dfs(candidates, target - candidates[i], res, cur, i);
+        cur.pop_back();
+        dfs(candidates, target, res, cur, i+1);
+    }
+};
