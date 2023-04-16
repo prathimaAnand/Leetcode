bcb873f04040185b0764722d27eb2b7955a35168
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-15 21:42:00 -0400

    Update Longest Palindromic Substring.cpp

--- C++ code/algorithms/Longest Palindromic Substring.cpp
+++ C++ code/algorithms/Longest Palindromic Substring.cpp
@@ -1,1 +1,9 @@
-
+/*
+Brut force technique :
+check with each character with each other
+Linear scan for every single substring = n * n^2 -> Time = O(n^3)
+To improve:
+keep a middle character and expand outward and check if they palindrome
+Edge case : It does not work for even palindromes
+fix it in code
+*/
