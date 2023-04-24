debf7dc4526bf29242f6df541df98f003a5665e5
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-04-24 14:07:01 -0400

    Update subtree of another tree.py

--- Blind75/trees/subtree of another tree.py
+++ Blind75/trees/subtree of another tree.py
@@ -1,1 +1,6 @@
-
+"""edge cases:
+2 helper functions - sameTree and isSubTree
+If both trees are null - same tree & is a sub tree
+Extra node for one of the sub trees - not same tree
+Is null tree a sub tree of other tree - yes
+"""
