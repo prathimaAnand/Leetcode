cb01fa73f0a0f1ad4109d5d90d7c8643306ec324
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-05-29 02:43:02 +0530

    Update number of 1 bits.py

--- Blind75/Bit manipullation/number of 1 bits.py
+++ Blind75/Bit manipullation/number of 1 bits.py
@@ -1,1 +1,23 @@
+'''Time - O(no. of bits) = constant O(1)
+Bruce force works here but Complicated logic can be figured and applied to reduce time complexity
+n = n& n(n-1)
+we can run the loop as many times as there are ones in the number
+'''
+#brut force
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result = 0
+        while n: 
+            result += n % 2
+             n = n >> 1
 
+        return result
+#logic
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result = 0
+        while n:
+            n &= (n-1)
+            result += 1
+
+        return result
