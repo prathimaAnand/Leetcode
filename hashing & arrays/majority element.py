e9d0e143d0f978ffa15d7885ed6e72e7c02b13ad
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-06-13 18:04:34 -0400

    Update majority element.py

--- hashing & arrays/majority element.py
+++ hashing & arrays/majority element.py
@@ -1,5 +1,13 @@
 # using Hashmap with O(n) memory - extra memory space
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, count = 0, 0
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            if count[n] > maxCount:
+                res = n
+        maxCount = max(count[n], maxCount)
+        return res 
 
 
 # Boyer Moore algo
@@ -7,7 +15,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         res, count = 0, 0
-
         for n in nums:
             if count == 0:
                 res = n
@@ -16,5 +23,8 @@ class Solution:
             else:
                 -1
         return res
-        
+    
+    
+    
+
         
