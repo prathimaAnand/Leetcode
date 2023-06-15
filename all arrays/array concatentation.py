9265d54ef0001f762f02facb973b045b302d4ded
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-06-14 23:58:09 -0400

    Update array concatentation.py

--- all arrays/array concatentation.py
+++ all arrays/array concatentation.py
@@ -1,3 +1,9 @@
 # iterate through every value and concatenate array to it
 #or
 # take an empty array and push values twice
+
+ans = []
+for i in range(2):
+  for n in nums:
+    ans.append(n)
+return ans
