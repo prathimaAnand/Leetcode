a1f737b1fb005b03d206deecf360b7ff20fe20fe
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-06-15 11:28:53 -0400

    Update array concatenation.cpp

--- C++ code/arrays/array concatenation.cpp
+++ C++ code/arrays/array concatenation.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans; // check if "list<int>" defination works or "std::vector<int>"
+        for (int i =0; i <=1 ; i++){
+            for(int n : nums){
+                ans.push_back(n);
+            }
+        }
+        return ans;
+    }
+};
