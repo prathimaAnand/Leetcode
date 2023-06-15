5647e364ebd74ec102e7fb19b919ab8dcd53cefb
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-06-15 00:00:03 -0400

    Update array concatentation.py

--- all arrays/array concatentation.py
+++ all arrays/array concatentation.py
@@ -2,8 +2,14 @@
 #or
 # take an empty array and push values twice
 
-ans = []
-for i in range(2):
-  for n in nums:
-    ans.append(n)
-return ans
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # run through array
+        # when end of array is detected concatenate once the same array
+        ans = [] # list for result
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
+        return ans
+        
+
