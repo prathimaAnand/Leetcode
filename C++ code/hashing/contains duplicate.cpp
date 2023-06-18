9a847d418a293c7552cb6681a9fe2b6a5511062a
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-06-18 16:59:16 -0400

    Update contains duplicate.cpp

--- C++ code/hashing/contains duplicate.cpp
+++ C++ code/hashing/contains duplicate.cpp
@@ -1,1 +1,16 @@
+// own code
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool checker;
+        // iterate through list & check if each val is equal or not
+        for(int i =0; i < nums.size(); i++){
+            if(nums[i] == nums[i+1]) //goes out of bounce & checks only consecutive elements
+                checker = true;
+            checker = false;
+        }
+        return checker;
+    }
+};
 
+// using hashset as tutorial
