5dd69207653104687998412c0f10d055828b9728
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-06-25 12:28:59 -0400

    Create longest consecutive sequence.py

---
+++ Blind75/Arrays & Hashing/longest consecutive sequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        longSet = set(nums)# longSet = {} # define set
+        longestLength = 0
+        initLength = 0
+
+        for n in nums: # iterate through list
+            
+            if (n - 1) not in longSet: #check if there is a n-1 number to check if start of sequene
+                initLength = 1
+
+                while (n+initLength) in longSet: # check consecutive numbers for sequence
+                    initLength += 1
+                longestLength = max(longestLength, initLength)
+
+        return longestLength
+
