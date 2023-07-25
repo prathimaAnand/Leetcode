7e7ea75a014701607e2a78ca277d93749b5df6c1
prathimaAnand <65678711+prathimaAnand@users.noreply.github.com>
2023-07-24 21:01:13 -0400

    Update single number.c

--- C- embedded/bit manipulation/single number.c
+++ C- embedded/bit manipulation/single number.c
@@ -1,1 +1,10 @@
-
+int singleNumber(int* nums, int numsSize){
+    // compare each value in array and check if its equal
+    int extra_var = 0;
+    for(int i = 0; i<numsSize; i++){
+        
+            extra_var = nums[i] ^ extra_var;
+        
+    }
+    return extra_var;
+}
